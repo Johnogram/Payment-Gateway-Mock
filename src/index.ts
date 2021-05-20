5a6d3d7d8454d1540e28bec0b8e78b9e03b02a70
John Minns <john.minns@missguided.com>
2021-05-20 22:22:30 +0100

    Getting server up and running

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+import cors from 'cors';
 import express from 'express';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
-export const server = express();
+const { NODE_PORT = 80, DISABLE_HTTPS = false } = process.env;
 
-const { NODE_PORT = 80 } = process.env;
+const server = express();
+server.use(cors());
 
-server.listen(NODE_PORT, () => {});
+if (DISABLE_HTTPS) {
+  http.createServer(server).listen(NODE_PORT);
+} else {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync('./certs/server.key'),
+        cert: fs.readFileSync('./certs/server.cert'),
+      },
+      server,
+    )
+    .listen(NODE_PORT);
+}
